7df09167934419e6cee4f21cd22aa4d409b22fbe
Annukul Thakran <annukulthakran@Annukuls-MacBook-Pro.local>
2023-01-17 11:58:36 +0530

    Landing page design + custom fonts added

--- src/views/screens/home.screen/home.screen.tsx
+++ src/views/screens/home.screen/home.screen.tsx
@@ -1,27 +1,71 @@
+import React from 'react';
+import backgroundImage from '../../../assets/images/main-background.jpg';
+import profileImage from '../../../assets/images/profile.jpg';
+
 export const HomeScreen = () => {
   return (
     <div>
-      <nav className="flex px-10 justify-between items-center border h-20">
-        <div>
-          <p className="text-2xl">Annukul</p>
+      <div
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundRepeat: 'no-repeat',
+          width: '100vw',
+          height: '100vh',
+          backgroundSize: 'cover',
+          backgroundAttachment: 'fixed',
+          backgroundPosition: '50%',
+        }}
+        className="flex">
+        <div className="w-3/5 flex flex-col items-center justify-center">
+          {/* <div>
+            <img
+              className="w-60 h-60"
+              style={{ borderRadius: 200 }}
+              src={profileImage}
+              alt="Logo"
+            />
+          </div> */}
+          <div className="w-3/4 mb-5">
+            <p
+              style={{ fontFamily: 'RubikMedium' }}
+              className="text-7xl text-amber-300">
+              Hey! I'm Annukul
+            </p>
+          </div>
+          <div className="w-3/4">
+            <p style={{ fontFamily: 'RubikMedium' }} className="text-3xl">
+              A Full Stack Developer experienced in building scalable and high
+              performant web applications.
+            </p>
+          </div>
         </div>
-        <div>
-          <ul className="flex w-96 justify-evenly h-20 items-center">
-            <li className="text-xl hover:bg-yellow-100 hover:drop-shadow-md px-4 py-2 cursor-pointer transition-shadow transition-colors duration-300">
-              Home
-            </li>
-            <li className="text-xl hover:bg-yellow-100 hover:drop-shadow-md px-4 py-2 cursor-pointer transition-shadow transition-colors duration-300">
-              Blog
-            </li>
-            <li className="text-xl hover:bg-yellow-100 hover:drop-shadow-md px-4 py-2 cursor-pointer transition-shadow transition-colors duration-300">
-              About
-            </li>
-            <li className="text-xl hover:bg-yellow-100 hover:drop-shadow-md px-4 py-2 cursor-pointer transition-shadow transition-colors duration-300">
-              Contact
-            </li>
-          </ul>
+        <div className="w-2/5">
+          <nav className="flex w-full h-4/5 items-center justify-start px-10 bg-amber-300">
+            <ul className="flex flex-col justify-between h-72">
+              <li
+                style={{ fontFamily: 'RubikMedium' }}
+                className="text-6xl font-bold text-violet-400">
+                About
+              </li>
+              <li
+                style={{ fontFamily: 'RubikMedium' }}
+                className="text-6xl font-mono font-bold text-violet-400">
+                Contact
+              </li>
+              <li
+                style={{ fontFamily: 'RubikMedium' }}
+                className="text-6xl font-mono font-bold text-violet-400">
+                Projects
+              </li>
+              <li
+                style={{ fontFamily: 'RubikMedium' }}
+                className="text-6xl font-mono font-bold text-violet-400">
+                Blog
+              </li>
+            </ul>
+          </nav>
         </div>
-      </nav>
+      </div>
     </div>
   );
 };
